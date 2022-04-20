253e401d6918b765d49e371b87889a33424ada46
dorothymea <meao0505@gmail.com>
2022-04-20 19:01:35 +0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,38 @@ import Tally from "./views/tally";
 import Tags from "./views/tags";
 import NoMatch from "./views/noMatch";
 import {TagEdit} from "./views/TagEdit";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  max-width: 520px;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-      <Router>
-            <Switch>
-            <Route path="/statistic">
-              <Statistic />
-            </Route>
-            <Route path="/tally">
-              <Tally/>
-            </Route>
-                <Route path="/tags/:id" exact={true}>
-                    <TagEdit />
-                </Route>
-            <Route path="/tags" exact={true}>
-                <Tags />
-            </Route>
-            <Redirect exact from="/" to="/tally" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-      </Router>
+      <AppWrapper>
+          <Router>
+              <Switch>
+                  <Route path="/statistic">
+                      <Statistic />
+                  </Route>
+                  <Route path="/tally">
+                      <Tally/>
+                  </Route>
+                  <Route path="/tags/:id" exact={true}>
+                      <TagEdit />
+                  </Route>
+                  <Route path="/tags" exact={true}>
+                      <Tags />
+                  </Route>
+                  <Redirect exact from="/" to="/tally" />
+                  <Route path="*">
+                      <NoMatch />
+                  </Route>
+              </Switch>
+          </Router>
+      </AppWrapper>
+
   );
 }
 
